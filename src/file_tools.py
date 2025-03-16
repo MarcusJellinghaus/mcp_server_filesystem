650b6d5378e3bed22c325ce26569b2b575858e6a
Marcus Jellinghaus <50779140+MarcusJellinghaus@users.noreply.github.com>
2025-03-16 20:00:15 +0100

    deprecated file removed

--- src/file_tools.py
+++
@@ -1,8 +0,0 @@
-"""
-DEPRECATED FILE - DO NOT USE
-
-This file has been replaced by the file_tools/ package.
-Import directly from src.file_tools package instead:
-
-from src.file_tools import get_project_dir, normalize_path, read_file, write_file, delete_file, list_files
-"""
